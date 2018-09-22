d09b9c6e9f520b1222be6c136eade93538ab8e21
Keith Tang <ktang081@uottawa.ca>
2018-09-22 17:32:10 -0400

    created basic structure for arrays

--- Assignment 1/src/Arrays/ArrayList.java
+++ Assignment 1/src/Arrays/ArrayList.java
@@ -1,5 +1,31 @@
 package Arrays;
+import java.util.Random;
+
 
 public class ArrayList {
+	
+	private static int anArray[];
+	
+	public static int[] array() {
+		anArray = new int[100];
+		for(int i=0; i < anArray.length; i++) {
+			anArray[i] = randFill();
+		}
+		return anArray;	
+	}
+	
+	public static int randFill() {
+		Random randInt = new Random();
+		int rand = randInt.nextInt();
+		return rand;
+	}
+	
+	public static void main(String args[]) {
+		array();
+		for(int i =0; i<anArray.length; i++) {
+			System.out.println(anArray[i]);
+		}
+	}
+	
 
 }
