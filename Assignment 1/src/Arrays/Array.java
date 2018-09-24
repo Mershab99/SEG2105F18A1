eb113653d95957e63fa31c823966d50a14c0953e
Keith Tang <ktang081@uottawa.ca>
2018-09-24 10:28:53 -0400

    Added sum counter for array

--- Assignment 1/src/Arrays/Array.java
+++ Assignment 1/src/Arrays/Array.java
@@ -13,13 +13,20 @@ public class Array {
 	public static void main(String args[]) {
 	
 		long startTime = System.nanoTime();
-		int[] anArray = new int[100000000];
+		int[] anArray = new int[10];
+		
+		int sum = 0;
+		
 		for(int i=0; i < anArray.length; i++) {
 			anArray[i] = randFill();
+			System.out.println(anArray[i]);
+			sum += anArray[i];
+			System.out.println(sum);
 		}
 		long endTime = System.nanoTime();	
 		
 		System.out.println("done");
+		//System.out.println(sum);
 		
 		System.out.println("The Array ran for: " + (endTime - startTime) + " nanoseconds");
 		
