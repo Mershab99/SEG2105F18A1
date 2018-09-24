a32cb5d409eb817d1282099f697d8de4a42a24ec
Keith Tang <ktang081@uottawa.ca>
2018-09-23 22:39:05 -0400

    Finished vector class

--- Assignment 1/src/Arrays/vect.java
+++ Assignment 1/src/Arrays/vect.java
@@ -1,5 +1,19 @@
 package Arrays;
 
+import java.util.Vector;
+import java.util.Random;
+
 public class vect {
+	
+	public static void main(String args[]) {
+		
+		Vector<Integer> v = new Vector<Integer>(); 
+		Random randInt = new Random();
+		
+		for(int i=0; i < 50000000; i++) {
+			v.add(randInt.nextInt(10));
+		}	
+		System.out.println("done");
+	}
 
 }
