bc9266f6ec24c490dd7d73e68a3a5ca5a77a72a7
mershab99 <mershab99@gmail.com>
2018-09-23 22:45:44 -0400

    Added timing to Vectors

--- Assignment 1/src/Arrays/vect.java
+++ Assignment 1/src/Arrays/vect.java
@@ -5,15 +5,28 @@ import java.util.Random;
 
 public class vect {
 	
+	
+	public static int TEST_CASES = 50000000;
+	
 	public static void main(String args[]) {
+		
+		
 		
 		Vector<Integer> v = new Vector<Integer>(); 
 		Random randInt = new Random();
 		
-		for(int i=0; i < 50000000; i++) {
+		long startTime = System.nanoTime();
+		
+		for(int i=0; i < TEST_CASES; i++) {
 			v.add(randInt.nextInt(10));
-		}	
+		}
+		
+		long endTime = System.nanoTime();
 		System.out.println("done");
+		
+		System.out.println("Ran for: " + (endTime - startTime));
+		
+		
 	}
 
 }
